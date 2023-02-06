8264d1839e0b80fb3fd47b86bb6259793a952e1a
hypersoph <53163574+hypersoph@users.noreply.github.com>
2023-02-06 16:29:51 -0500

    auth reducer

--- src/app/auth/reducers/index.ts
+++ src/app/auth/reducers/index.ts
@@ -2,20 +2,36 @@ import {
   ActionReducer,
   ActionReducerMap,
   createFeatureSelector,
+  createReducer,
   createSelector,
-  MetaReducer
+  MetaReducer,
+  on
 } from '@ngrx/store';
 import { environment } from '../../../environments/environment';
+import { AuthActions } from '../action-types';
 
 export const authFeatureKey = 'auth';
 
-export interface State {
+export interface AuthState {
 
 }
 
-export const reducers: ActionReducerMap<State> = {
+export const initialAuthState: AuthState = {
+  user: undefined
+}
+
+export const reducers: ActionReducerMap<AuthState> = {
 
 };
 
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+export const metaReducers: MetaReducer<AuthState>[] = !environment.production ? [] : [];
+
+export const authReducer = createReducer(
+  initialAuthState,
+  on(AuthActions.login, (state, action) => {
+    return {
+      user: action.user
+    }
+  })
+)
